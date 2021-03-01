83a77bc90bfc8249e31150170264e95cab80ea6f
Kip Cole <kipcole9@gmail.com>
2021-03-01 12:17:49 +0800

    Add Format.grammatical_case

--- lib/cldr/unit/format.ex
+++ lib/cldr/unit/format.ex
@@ -31,17 +31,59 @@ defmodule Cldr.Unit.Format do
       |> Map.merge(module.grammatical_features(locale))
       |> Map.fetch!(:case)
 
-    Cldr.Unit.Format.reduce(unit, fn
+    traverse(unit, fn
       {:unit, unit} ->
-        {:unit, {:compound, unit}}
-      {:per, {compound, {_, left}, {_, right}}} ->
-        {:per, {{features.per[0], left}, {features.per[1], right}}}
+        {:compound, unit}
+
+      # For :per we return a tuple which is
+      # a consistent representation. Each element
+      # is a list.
+      {:per, {left, right}} when is_list(left) and is_list(right) ->
+        {left, right}
+
+      {:per, {left, {_, right}}} when is_list(left) ->
+        {left, [{features.per[1], right}]}
+
+      {:per, {{_, left}, right}} when is_list(right) ->
+        {[{features.per[0], left}], right}
+
+      {:per, {{_, left}, {_, right}}} ->
+        {[{features.per[0], left}], [{features.per[1], right}]}
+
+      # :times will always have a unit as the
+      # `left`, and a unit or a list as the `right`
+
+      # If the `right` is a list then its a `:times`
+      # in which case there is no transform to apply
+      # and all locales are marked as `:compound` anyway.
+      {:times, {left, right}} when is_list(left) and is_list(right) ->
+        left ++ right
+
+      {:times, {{_, left}, right}} when is_list(right) ->
+        [{features.times[0], left} | right]
+
+      {:times, {left, {_, right}}} when is_list(left) ->
+        left ++ [{features.times[1], right}]
+
       {:times, {{_, left}, {_, right}}} ->
-        {:times, {{features.times[0], left}, {features.times[1], right}}}
+        [{features.times[0], left}, {features.times[1], right}]
+
+      # :power will always have a unit as the
+      # `left`, and a unit or a prefix as the `right`
+
+      # If the `right` is a list then its a `:times`
+      # in which case there is no transform to apply
+      # and all locales are marked as `:compound` anyway.
+      {:power, {{_, left}, right}} when is_list(right) ->
+        [{features.power[0], left} | right]
+
       {:power, {{_, left}, {_, right}}} ->
-        {:power, {{features.power[0], left}, {features.power[1], right}}}
+        [{features.power[0], left}, {features.power[1], right}]
+
+      # :prefix can only have a prefix and
+      # a unit so apply the transform to both
       {:prefix, {{_, left}, {_, right}}} ->
-        {:prefix, {{features.prefix[0], left}, {features.prefix[1], right}}}
+        [{features.prefix[0], left}, {features.prefix[1], right}]
     end)
   end
 
@@ -76,46 +118,46 @@ defmodule Cldr.Unit.Format do
   The result returned from `fun/1`
 
   """
-  def reduce(%Unit{base_conversion: {left, right}}, fun) when is_function(fun) do
-    fun.({:per, {do_reduce(left, fun), do_reduce(right, fun)}})
+  def traverse(%Unit{base_conversion: {left, right}}, fun) when is_function(fun) do
+    fun.({:per, {do_traverse(left, fun), do_traverse(right, fun)}})
   end
 
-  def reduce(%Unit{base_conversion: conversion}, fun) when is_function(fun) do
-    do_reduce(conversion, fun)
+  def traverse(%Unit{base_conversion: conversion}, fun) when is_function(fun) do
+    do_traverse(conversion, fun)
   end
 
-  defp do_reduce([{unit, _}], fun) do
-    do_reduce(unit, fun)
+  defp do_traverse([{unit, _}], fun) do
+    do_traverse(unit, fun)
   end
 
-  defp do_reduce([head | rest], fun) do
-    fun.({:times, {do_reduce(head, fun), do_reduce(rest, fun)}})
+  defp do_traverse([head | rest], fun) do
+    fun.({:times, {do_traverse(head, fun), do_traverse(rest, fun)}})
   end
 
-  defp do_reduce({unit, _}, fun) do
-    do_reduce(unit, fun)
+  defp do_traverse({unit, _}, fun) do
+    do_traverse(unit, fun)
   end
 
   # String decomposition
   for {power, exp} <- @power do
     power_unit = String.to_existing_atom("power#{exp}")
-    defp do_reduce(unquote(power) <> "_" <> unit, fun) do
-      fun.({:power, {fun.({:unit, unquote(power_unit)}), do_reduce(unit, fun)}})
+    defp do_traverse(unquote(power) <> "_" <> unit, fun) do
+      fun.({:power, {fun.({:unit, unquote(power_unit)}), do_traverse(unit, fun)}})
     end
   end
 
   for {prefix, exp} <- @prefix do
     prefix_unit = String.to_existing_atom("10p#{exp}" |> String.replace("-", "_"))
-    defp do_reduce(unquote(prefix) <> unit, fun) do
+    defp do_traverse(unquote(prefix) <> unit, fun) do
       fun.({:prefix, {fun.({:unit, unquote(prefix_unit)}), fun.({:unit, String.to_existing_atom(unit)})}})
     end
   end
 
-  defp do_reduce(unit, fun) when is_binary(unit) do
+  defp do_traverse(unit, fun) when is_binary(unit) do
     fun.({:unit, String.to_existing_atom(unit)})
   end
 
-  defp do_reduce(unit, fun) when is_atom(unit) do
+  defp do_traverse(unit, fun) when is_atom(unit) do
     fun.({:unit, unit})
   end
 
