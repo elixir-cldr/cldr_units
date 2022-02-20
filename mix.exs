4ba696580d6746d422eb05a954a69b61bf3fab27
Kip Cole <kipcole9@gmail.com>
2022-02-21 07:05:05 +0800

    Update ex_cldr_numbers and ex_cldr_lists

--- mix.exs
+++ mix.exs
@@ -38,10 +38,8 @@ defmodule Cldr.Units.Mixfile do
 
   defp deps do
     [
-      # {:ex_cldr_numbers, "~> 2.23"},
-      # {:ex_cldr_lists, "~> 2.9"},
-      {:ex_cldr_numbers, path: "../cldr_numbers", override: true},
-      {:ex_cldr_lists, path: "../cldr_lists"},
+      {:ex_cldr_numbers, "~> 2.25"},
+      {:ex_cldr_lists, "~> 2.10"},
 
       {:ratio, "~> 2.4"},
       {:decimal, "~> 1.6 or ~> 2.0"},
