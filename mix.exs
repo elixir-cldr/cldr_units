429e9055f49d72dbcdfc8bdef64003b38c317308
Kip Cole <kipcole9@gmail.com>
2023-03-22 06:10:39 +0800

    Revert mix.exs changes

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Cldr.Units.Mixfile do
   use Mix.Project
 
-  @version "3.16.0-dev"
+  @version "3.15.0"
 
   def project do
     [
@@ -41,7 +41,7 @@ defmodule Cldr.Units.Mixfile do
     [
       {:ex_cldr_numbers, "~> 2.28"},
       {:ex_cldr_lists, "~> 2.10"},
-      {:ratio, "~> 2.4 or ~> 3.0 or ~> 4.0 or ~> 4.0-rc"},
+      {:ratio, "~> 2.4 or ~> 3.0 or ~> 4.0"},
       {:decimal, "~> 1.6 or ~> 2.0"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.18", optional: true, runtime: false},
