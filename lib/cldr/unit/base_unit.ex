726b707a4c3b3cb72f1244f8a6c89156f44f3619
Kip Cole <kipcole9@gmail.com>
2024-03-18 14:17:41 +1100

    Fix canonical_base_name

--- lib/cldr/unit/base_unit.ex
+++ lib/cldr/unit/base_unit.ex
@@ -358,17 +358,39 @@ defmodule Cldr.Unit.BaseUnit do
     do_reduce_factors({[sub_unit_1 | rest_1], [sub_unit_2 | rest_2]})
   end
 
-  # No power units involved so we need to move along.
-  # THe list of units is always in a canonical order.
-  def do_reduce_factors({[unit_1 | rest_1], [unit_2 | rest_2]}) do
-    cond do
-      compare(unit_1, unit_2) == :lt ->
-        {reduced_1, reduced_2} = do_reduce_factors({rest_1, [unit_2 | rest_2]})
-        {[unit_1 | reduced_1], reduced_2}
+  # We still have to check for embedded power units like :square_meter
+  # which can be reduced against :meter.
 
-      compare(unit_1, unit_2) == :gt ->
-        {reduced_1, reduced_2} = do_reduce_factors({[unit_1 | rest_1], rest_2})
-        {reduced_1, [unit_2 | reduced_2]}
+  def do_reduce_factors({[unit_1 | rest_1], [unit_2 | rest_2]}) do
+    {name_1, power_1} = Conversion.name_and_power(unit_1)
+    {name_2, power_2} = Conversion.name_and_power(unit_2)
+
+    if name_1 == name_2 do
+      cond do
+        # if they are the same power then omit both
+        power_1 == power_2 ->
+          do_reduce_factors({rest_1, rest_2})
+
+        power_1 > power_2 ->
+          power_1 = power_1 - power_2
+          [new_unit] = Conversion.base_unit(name_1, power_1)
+          do_reduce_factors({[new_unit | rest_1], rest_2})
+
+        power_1 < power_2 ->
+          power_2 = power_2 - power_1
+          [new_unit] = Conversion.base_unit(name_1, power_2)
+          do_reduce_factors({rest_1, [new_unit | rest_2]})
+      end
+    else
+      cond do
+        compare(unit_1, unit_2) == :lt ->
+          {reduced_1, reduced_2} = do_reduce_factors({rest_1, [unit_2 | rest_2]})
+          {[unit_1 | reduced_1], reduced_2}
+
+        compare(unit_1, unit_2) == :gt ->
+          {reduced_1, reduced_2} = do_reduce_factors({[unit_1 | rest_1], rest_2})
+          {reduced_1, [unit_2 | reduced_2]}
+      end
     end
   end
 
